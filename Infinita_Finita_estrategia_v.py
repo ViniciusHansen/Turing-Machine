5dcb6117a95f31758ae79554ba465fdd2c9d0ef8
Vinicius Hansen <vinicius.carlo.hansen@gmail.com>
2024-04-15 17:27:17 -0300

    tirar duvida com a prof

--- Infinita_Finita_estrategia_v.py
+++ Infinita_Finita_estrategia_v.py
@@ -24,14 +24,16 @@ class TuringMachine:
                 new_symbol = str(parts[2])
                 direction = str(parts[3])
                 new_state = str(parts[4]) if parts[4] == "halt-accept" else str(int(parts[4]) + 1000)
-
-                if new_symbol == '_' and current_symbol == '_' and direction == 'l' and int(current_state) > 900:
-                    # new_symbol = '!'
-                    # current_symbol = '!'
-                    self.transitions[(current_state, '!')] = ('!', direction, new_state)
-                elif current_symbol == '_' and direction == 'l' and int(current_state) > 900:
-                    self.transitions[(current_state, '!')] = (new_symbol, direction, new_state)
                 
+                if current_symbol == '_' and new_symbol == '_' and direction == 'l' and int(new_state) > 900:
+                    self.transitions[(current_state, 'v')] = ('v', direction, new_state)
+                elif current_symbol == '_' and new_symbol == '_' and direction == 'r' and int(new_state) > 900:
+                    current_symbol = 'v'
+                    new_symbol = 'v'
+                elif current_symbol == '_' and int(new_state) > 900:
+                    current_symbol = 'v'
+                elif new_symbol == '_' and int(new_state) > 900:
+                    new_symbol = 'v'
                 # primeiramente vamos inserir um simbolo especial no começo da fita. Veja figura 1.
                 # <current state> <current symbol> <new symbol> <direction> <new state>
                 # 0  0 0 r 0
@@ -55,12 +57,7 @@ class TuringMachine:
                 # agora vamos adicionar os novos estados
                 self.transitions[('0', '0')] = ('0', 'r', '0')
                 self.transitions[('0', '1')] = ('1', 'r', '0')
-                self.transitions[('0', '_')] = ('!', '*', '1')
-                
-                self.transitions[('1', '!')] = ('#', 'r', '1!')
-                self.transitions[('1!', '_')] = ('!', '*', '1w')
-                self.transitions[('1w', '!')] = ('!', 'l', '1w')
-                
+                self.transitions[('0', '_')] = ('_', 'l', '1')
                 self.transitions[('1', '1')] = ('#', 'r', '11')
                 self.transitions[('1', '0')] = ('#', 'r', '10')
                 self.transitions[('11', '_')] = ('1', '*', '1w')
@@ -77,8 +74,8 @@ class TuringMachine:
                 self.transitions[('2w', '#')] = ('#', 'l', '2')
                 self.transitions[('2', '#')] = ('#', 'r', '1000')
                 
-                # estados da figura 2  # X é todos os estados do programa original
-                ## x    # # r 100 
+                # estados da figura 2# X é todos os estados do programa original
+                # x    # # r 100 
                 # 100  0 0 r 100 
                 # 100  1 1 r 100
                 # 100  v v r 100
@@ -101,7 +98,8 @@ class TuringMachine:
                 # 200w v v l 200w
                 # 200w $ $ l 200
                 # 200  # # r 201
-                ## 201  $ v * x
+                # 201  $ v * x
+                
                 # fig 2
                 self.transitions[('100', 'x')] = ('0', 'r', '100')
                 self.transitions[('100', '0')] = ('0', 'r', '100')
@@ -126,9 +124,9 @@ class TuringMachine:
                 self.transitions[('200w', 'v')] = ('v', 'l', '200w')
                 self.transitions[('200w', '$')] = ('$', 'l', '200')
                 self.transitions[('200', '#')] = ('#', 'r', '201')
-                # self.transitions[('1001', '#')] = ('#', 'r', '1001')
-                # self.transitions[('1001', '#')] = ('#', 'r', '100')
-                # self.transitions[('201', '$')] = ('v', '*', '1001')
+                self.transitions[('1001', '#')] = ('#', 'r', '1001')
+                self.transitions[('1001', '#')] = ('#', 'r', '100')
+                self.transitions[('201', '$')] = ('v', '*', '1001')
                 
                 
 
@@ -141,17 +139,17 @@ class TuringMachine:
                     # mover a fita pra direita novamente e inserir o simbolo em branco
                     # exemplo: #1010  --->  #_1010
                     
-                estados = list(set(state for state, _ in self.transitions.keys()))
-                # print(estados)
-                # print(self.transitions)
-                for estado in estados:
-                    try:
-                        if int(estado) >= 1000:
-                            self.transitions[(str(estado), '#')] = ('#', 'r', str(estado))
-                            self.transitions[(str(estado), '#')] = ('#', 'r', '100')
-                            self.transitions[('201', '$')] = ('v', '*', str(estado))
-                    except:
-                        pass
+                # estados = list(set(state for state, _ in self.transitions.keys()))
+                # # print(estados)
+                # # print(self.transitions)
+                # for estado in estados:
+                #     try:
+                #         if int(estado) >= 1000:
+                #             self.transitions[('1001', '#')] = ('#', 'r', '1001')
+                #             self.transitions[('1001', '#')] = ('#', 'r', '100')
+                #             self.transitions[('201', '$')] = ('v', '*', '1001')
+                #     except:
+                #         pass
 
 
             
@@ -166,9 +164,8 @@ class TuringMachine:
                 file.write(line)
 
 
-
-# O arquivo sameamount10.txt é de uma máquina com fita duplamente infinita 
-# que aceita a linguagem das sequências binárias que possuem a mesma quantidade de 0s e de 1s.
+# O arquivo odd.txt é de uma máquina com fita limitada à esquerda (modelo de Sipser) 
+# que aceita a linguagem das sequências binárias de comprimento ímpar.
 
 print(f"Simulando a maquina finita na infinita...")
 MT1 = TuringMachine('sameamount10.in')
